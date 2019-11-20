977c7664b77db3a22cc910af1197062ae8dd0c56
Dani Treisman <danitreisman@gmail.com>
2019-11-20 12:57:47 -0600

    delete these

--- R/alphabet.R
+++
@@ -1,7 +0,0 @@
-alphabet <-
-function(n) {
-  
-  letters <- c("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z")
-  sample(letters, n, replace = T)
-  print("Cool, it works")
-}
